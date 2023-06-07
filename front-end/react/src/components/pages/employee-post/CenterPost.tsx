4b62d42128b449f2c1e810bfd6e774109ee90c2c
Huy Thai <huythai2001cs@gmail.com>
2023-06-08 00:03:39 +0700

    fix

--- front-end/react/src/components/pages/employee-post/CenterPost.tsx
+++ front-end/react/src/components/pages/employee-post/CenterPost.tsx
@@ -1,30 +1,38 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AddPostModal from "../../mini-component/add-post-modal/AddPostModal";
 import "./styles.scss";
 import AddNote from "./AddNote";
 import { useTasksiteContext } from "../../../contexts/tasksiteContext";
 import NoteItem from "./NoteItem";
+import { AuthContext } from "../../../contexts/authContext";
 
 interface Props {
   idProfile?: number | undefined;
 }
 
 const CenterPost: React.FC<Props> = ({ idProfile }) => {
+  const {
+    authState: { account },
+  } = useContext(AuthContext);
+
   const { getAllNote, noteList } = useTasksiteContext();
   const [show, setShow] = useState(false);
   const [isFilter, setShowFilter] = useState(false);
   useEffect(() => {
     getAllNote(idProfile);
   }, [idProfile]);
+
   return (
     <>
-      <AddPostModal
-        element={{
-          setShow: setShow,
-          isFilter: isFilter,
-          setShowFilter: setShowFilter,
-        }}
-      />
+      {(!idProfile || idProfile === account.id) && (
+        <AddPostModal
+          element={{
+            setShow: setShow,
+            isFilter: isFilter,
+            setShowFilter: setShowFilter,
+          }}
+        />
+      )}
       <div className="d-flex" style={{ flexDirection: "column-reverse" }}>
         {noteList.map((note: any, id: number) => (
           <NoteItem item={note} key={id} />
